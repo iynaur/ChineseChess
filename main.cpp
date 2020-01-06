a17d5dcb86f60acfe263cf2a34aef444b2c3cdf2
毅 阮 <iynaur@hotmail.com>
2020-01-06 22:23:11 +0800

    fix win msvc compile

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
-#include "mainwidget.h"#include <QApplication>#include "multigame.h"int main(int argc, char *argv[]){    QApplication a(argc, argv);    MainWidget w;    //MultiGame w ;    w.show();    return a.exec();}
+#include "mainwidget.h"
+#include <QApplication>
+#include "multigame.h"
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWidget w;
+    //MultiGame w ;
+    w.show();
+    return a.exec();
+}
