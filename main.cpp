e70d061994bd62e72a6fc51b77f4089eecea36c3
ruanyi <iynaur87@sina.com>
2020-01-06 09:38:25 +0800

    merge rich ui to main

--- main.cpp
+++ main.cpp
@@ -1,23 +1,14 @@
+#include "mainwidget.h"
 #include <QApplication>
-#include"board.h"
 #include "multigame.h"
-#include "singlegame.h"
-#include "netgame.h"
-#include <QMessageBox>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    //MultiGame board;
-    //Board board;
-    //SingleGame board ;
-    QMessageBox::StandardButton ret ;
-    ret = QMessageBox::question(NULL,"server or client","as server");
-    bool server = false ;
-    if(ret == QMessageBox::Yes){
-        server = true ;
-    }
-    NetGame board(server);
-    board.show();
+    MainWidget w;
+    //MultiGame w ;
+    w.show();
+
     return a.exec();
 }
